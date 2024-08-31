19e7944beb80f256d6a88a399b11a5b0df0fb517
David Inga <davidsingaldev@gmail.com>
2024-08-31 08:50:15 +0200

    scaffold

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// const PluginTailwindCSS = require('prettier-plugin-tailwindcss');
+
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+module.exports = config;
