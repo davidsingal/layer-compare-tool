363405cef47557fd30b556d045705bedc2c14b36
David Inga <david@transparenc.io>
2024-09-03 16:53:56 +0200

    do not resize header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from './ui/button';
 
 const Header: React.FC = () => {
   return (
-    <header className="flex h-16 w-full items-center gap-2 border-b px-6">
+    <header className="flex h-16 w-full shrink-0 items-center gap-2 border-b px-6">
       <h1 className="text-xl font-bold">Raster, COG, WMS Comparison Tool</h1>
       <div className="text-foreground/50">by David Inga</div>
       <div className="flex flex-1 items-center justify-end">
